6212bfaf64e355cf409435df054631d7daede970
Kyle Friel <businessfriel@gmail.com>
2021-07-30 01:47:28 -0400

    Customer profile imgs.

--- src/components/Dashboard/CustomersList/CustomersList.js
+++ src/components/Dashboard/CustomersList/CustomersList.js
@@ -3,12 +3,23 @@ import UiContext from "../../../store/ui-context";
 
 import classes from "./CustomersList.module.scss";
 
+import BrooklynJPG from "./../../../assets/BrooklynJPG.jpg";
+import JacobJPG from "./../../../assets/JacobJPG.jpg";
+import LeslieJPG from "./../../../assets/LeslieJPG.jpg";
+import TheresaJPG from "./../../../assets/TheresaJPG.jpg";
+import AlbertJPG from "./../../../assets/AlbertJPG.jpg";
+
 const customersData = [
-    { id: 1, customerName: "Brooklyn Simmons", email: "brooklyn.s@gmail.com", img: "" },
-    { id: 2, customerName: "Jacob Jones", email: "jacob.j@gmail.com", img: "" },
-    { id: 3, customerName: "Leslie Alexander", email: "leslie.a@gmail.com", img: "" },
-    { id: 4, customerName: "Theresa Webb", email: "theresa.w@gmail.com", img: "" },
-    { id: 5, customerName: "Albert Flores", email: "albert.f@gmail.com", img: "" },
+    {
+        id: 1,
+        customerName: "Brooklyn Simmons",
+        email: "brooklyn.s@gmail.com",
+        img: BrooklynJPG,
+    },
+    { id: 2, customerName: "Jacob Jones", email: "jacob.j@gmail.com", img: JacobJPG },
+    { id: 3, customerName: "Leslie Alexander", email: "leslie.a@gmail.com", img: LeslieJPG },
+    { id: 4, customerName: "Theresa Webb", email: "theresa.w@gmail.com", img: TheresaJPG },
+    { id: 5, customerName: "Albert Flores", email: "albert.f@gmail.com", img: AlbertJPG },
 ];
 
 const CustomersList = () => {
@@ -24,7 +35,13 @@ const CustomersList = () => {
             <div className={classes.list}>
                 {customersData.map(({ id, customerName, email, img }) => (
                     <div className={classes.list__item} key={id}>
-                        <div className={classes.list__item__img}>{img}</div>
+                        <div className={classes.list__item__img}>
+                            <img
+                                src={img}
+                                alt={`${customerName} head shot`}
+                                className={classes.img}
+                            />
+                        </div>
                         <div className={classes.list__item__info}>
                             <span className={`${classes.name} ${themeClass}`}>{customerName}</span>
                             <span className={`${classes.email} ${themeClass}`}>{email}</span>
